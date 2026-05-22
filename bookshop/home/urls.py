49b599afb84e1fd4bfcc040bde4927a6bafe8c63
Maqsudbek <dehkanaliev@gmail.com>
2026-05-22 08:58:55 +0500

    The last submit!

--- bookshop/home/urls.py
+++ bookshop/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, books_list, add_book
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='home'),
+    path('books/', books_list, name='book_list'),
+    path('new-book/', add_book, name='new-book')
 ]
--- bookshop/home/views.py
+++ bookshop/home/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Book
 
 def home(request):
     
     return render(request, 'index.html')
 
 def books_list(request):
-    return render(request, 'book_list.html')
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        summary = request.POST.get('summary')
+        author = request.POST.get('author')
+        price = request.POST.get('price')
+        photo = request.FILES.get('photo')
+        
+        new_book = Book(
+            title=title,
+            summary=summary,
+            author=author,
+            price=price,
+            photo=photo
+        )
+        
+        new_book.save()
+        
+        return redirect('book_list')
+    
+    return render(request, 'add_book.html')
