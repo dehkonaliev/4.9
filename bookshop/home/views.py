f10c54bfb46f981f3011703c12d7ee8cfa47e094
Maqsudbek <dehkanaliev@gmail.com>
2026-05-22 08:00:41 +0500

    The books list added!

--- bookshop/home/views.py
+++ bookshop/home/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    
     return render(request, 'index.html')
+
+def books_list(request):
+    return render(request, 'book_list.html')
